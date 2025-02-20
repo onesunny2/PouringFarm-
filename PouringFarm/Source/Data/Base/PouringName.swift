ce6170b0b72ada49217983f96db926acb5f0f077
onesunny2 <ippne996@gmail.com>
2025-02-20 20:00:12 +0900

    [feat] 푸링이 정보 (이름, 레벨에 따른 이미지명) 설정

--- PouringFarm/Source/Data/Base/PouringName.swift
+++ PouringFarm/Source/Data/Base/PouringName.swift
@@ -7,14 +7,32 @@
 
 import Foundation
 
-enum Pouring {
-    case 따끔푸링
-    case 방실푸링
-    case 반짝푸링
+enum PouringName: String {
+    case 따끔푸링 = "따끔따끔 푸링이"
+    case 방실푸링 = "방실방실 푸링이"
+    case 반짝푸링 = "반짝반짝 푸링이"
+    
+    var introduction: String {
+        
+        switch self {
+        case .따끔푸링: return pouringIntro[0]
+        case .방실푸링: return pouringIntro[1]
+        case .반짝푸링: return pouringIntro[2]
+        }
+    }
+    
+    // 현재 레벨에 따른 이미지명
+    func imageName(_ level: Int) -> String {
+        switch self {
+        case .따끔푸링: return "1-\(level)"
+        case .방실푸링: return "2-\(level)"
+        case .반짝푸링: return "3-\(level)"
+        }
+    }
 }
 
-enum PouringName {
-    case 푸링이_이름
-    case 푸링이_번호
-    case 푸링이
-}
+private let pouringIntro: [String] = [
+    "",
+    "",
+    ""
+]
