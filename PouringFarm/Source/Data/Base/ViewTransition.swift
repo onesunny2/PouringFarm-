c4b5098ad1d253f681fd06d8f629f3ef1a3927a6
onesunny2 <ippne996@gmail.com>
2025-02-21 00:40:28 +0900

    [feat] 화면 전환 경우의 수 열거형으로 구현

---
+++ PouringFarm/Source/Data/Base/ViewTransition.swift
@@ -0,0 +1,13 @@
+//
+//  ViewTransition.swift
+//  PouringFarm
+//
+//  Created by Lee Wonsun on 2/21/25.
+//
+
+import Foundation
+
+enum ViewTransition {
+    case navigation
+    case changeRootVC
+}
