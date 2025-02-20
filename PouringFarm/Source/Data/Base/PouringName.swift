af9995abbc0215ca491bebe10c78f49a73b0bc9c
onesunny2 <ippne996@gmail.com>
2025-02-20 19:55:03 +0900

    [chore] 10레벨 로직을 위해 이미지 에셋 1개 복사

---
+++ PouringFarm/Source/Data/Base/PouringName.swift
@@ -0,0 +1,20 @@
+//
+//  PouringSet.swift
+//  PouringFarm
+//
+//  Created by Lee Wonsun on 2/20/25.
+//
+
+import Foundation
+
+enum Pouring {
+    case 따끔푸링
+    case 방실푸링
+    case 반짝푸링
+}
+
+enum PouringName {
+    case 푸링이_이름
+    case 푸링이_번호
+    case 푸링이
+}
