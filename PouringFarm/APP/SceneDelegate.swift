dad2963ce268237f73cea771d3d8f82d07c0785e
onesunny2 <ippne996@gmail.com>
2025-02-21 11:35:15 +0900

    [feat] 푸링이선택화면 VC 파일 생성 및 선택 유무에 따라 시작화면 분기점 설정 (sceneDelegate)

--- PouringFarm/APP/SceneDelegate.swift
+++ PouringFarm/APP/SceneDelegate.swift
@@ -13,12 +13,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        @PouringDefaults(key: .최초_푸링이_선택유무, empty: false) var isSelected
+
         guard let scene = (scene as? UIWindowScene) else { return }
+        
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = UINavigationController(rootViewController: HomeViewController())
+        window?.rootViewController = UINavigationController(rootViewController: isSelected ?  HomeViewController() : SelectPouringViewController())
         window?.makeKeyAndVisible()
     }
 
---
+++ PouringFarm/Source/Presentation/SelectPouring/SelectPouringViewController/SelectPouringViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SelectPouringViewController.swift
+//  PouringFarm
+//
+//  Created by Lee Wonsun on 2/21/25.
+//
+
+import UIKit
+
+final class SelectPouringViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .prBackground
+    }
+
+}
