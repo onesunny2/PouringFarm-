340665fd7d5979dd801130c1f96ceaef6dd40c73
onesunny2 <ippne996@gmail.com>
2025-02-21 13:31:49 +0900

    [design] BaseUIImageView 생성

--- PouringFarm/Resource/CustomView/BaseUIImageView.swift
+++ PouringFarm/Resource/CustomView/BaseUIImageView.swift
@@ -5,4 +5,20 @@
 //  Created by Lee Wonsun on 2/21/25.
 //
 
-import Foundation
+import UIKit
+
+final class BaseUIImageView: UIImageView {
+    
+    init(_ imgName: String) {
+        super.init(frame: .zero)
+        
+        self.image = UIImage(named: imgName)
+        self.contentMode = .scaleAspectFit
+        self.backgroundColor = .clear
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
