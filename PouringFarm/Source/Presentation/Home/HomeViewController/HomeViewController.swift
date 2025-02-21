d64f5844ba73b82b5217e3af021d06ead4f0079f
onesunny2 <ippne996@gmail.com>
2025-02-21 10:55:14 +0900

    [text] projectedValue 작동유무 테스트

--- PouringFarm/Source/Presentation/Home/HomeViewController/HomeViewController.swift
+++ PouringFarm/Source/Presentation/Home/HomeViewController/HomeViewController.swift
@@ -22,25 +22,47 @@ final class HomeViewController: UIViewController {
     
         return button
     }()
+    
+    private let saveName: UILabel = {
+        let name = UILabel()
+        name.font = .systemFont(ofSize: 15, weight: .medium)
+        
+        return name
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .prBackground
         
-        @PouringDefaults(key: .대장이름, empty: "아직없음") var daejang
-        print(daejang)
-        
         view.addSubview(button)
+        view.addSubview(saveName)
         button.snp.makeConstraints { make in
             make.center.equalTo(view.safeAreaLayoutGuide)
             make.size.equalTo(150)
         }
         
+        saveName.snp.makeConstraints { make in
+            make.top.equalTo(button.snp.bottom).offset(20)
+            make.centerX.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        bindData()
+    }
+    
+    func bindData() {
+        @PouringDefaults(key: .대장이름, empty: "아직없음") var daejang
+        print(daejang)
+        print($daejang)
+        
         button.rx.tap
             .bind { _ in
-                daejang = "김기영"
+                daejang = "zzamong"
             }.disposed(by: disposeBag)
+        
+        $daejang
+            .bind(to: saveName.rx.text)
+            .disposed(by: disposeBag)
     }
 
 }
