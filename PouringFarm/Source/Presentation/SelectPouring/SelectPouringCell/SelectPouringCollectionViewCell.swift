4d9b582d7f97b159460aa0cd75cf53329ca3a104
onesunny2 <ippne996@gmail.com>
2025-02-21 13:38:56 +0900

    [design] cell 내 푸링이 이미지 자리 구현

--- PouringFarm/Source/Presentation/SelectPouring/SelectPouringCell/SelectPouringCollectionViewCell.swift
+++ PouringFarm/Source/Presentation/SelectPouring/SelectPouringCell/SelectPouringCollectionViewCell.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import SnapKit
 
-final class SelectPouringCollectionViewCell: UICollectionViewCell {
-    
+final class SelectPouringCollectionViewCell: UICollectionViewCell, BaseCell {
+
     static let id = "SelectPouringCollectionViewCell"
     
+    private let pouringImage: BaseUIImageView
+    private let pouringName = UILabel()
+    
     override init(frame: CGRect) {
+        
+        pouringImage = BaseUIImageView("noImage")
+        
         super.init(frame: frame)
         
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    func configureHierarchy() {
+        contentView.addSubview(pouringImage)
+    }
+    
+    func configureLayout() {
+        pouringImage.snp.makeConstraints {
+            $0.top.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(self.frame.width)
+        }
+    }
+    
+    func configureView() {
         backgroundColor = .white
     }
     
