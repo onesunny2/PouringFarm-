9c0e576595b4b6477082b32fde098be2e1cacf9d
onesunny2 <ippne996@gmail.com>
2025-02-23 00:32:08 +0900

    [chore] PouringInfo 열거형 내 PouringDefaults 프로퍼티래퍼 케이스별 프로퍼티 생성

--- PouringFarm/Resource/Manager/PouringDefaults.swift
+++ PouringFarm/Resource/Manager/PouringDefaults.swift
@@ -48,4 +48,11 @@ enum SavingInfo: String {
     case 현재_푸링이_밥갯수 = "pouringBab"
     case 현재_푸링이_물갯수 = "pouringWater"
     case 최초_푸링이_선택유무 = "isSelectedPouringFirst"
+    
+    @PouringDefaults(key: .대장이름, empty: "대장") static var daejang
+    @PouringDefaults(key: .푸링이종류, empty: PouringName.헤실푸링.rawValue) static var currentPouring
+    @PouringDefaults(key: .현재_푸링이_레벨, empty: 1) static var currentLevel
+    @PouringDefaults(key: .현재_푸링이_밥갯수, empty: 0) static var currentBab
+    @PouringDefaults(key: .현재_푸링이_물갯수, empty: 0) static var currentWater
+    @PouringDefaults(key: .최초_푸링이_선택유무, empty: false) static var isSelectFirst
 }
