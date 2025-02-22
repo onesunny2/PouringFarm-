b01c2534d55895b5b7db358fbae3501a29ec57de
onesunny2 <ippne996@gmail.com>
2025-02-22 14:55:13 +0900

    [chore] 가공된 푸링이 정보를 튜플에서 새로운 구조체로 타입 사용할 수 있도록 교체

--- PouringFarm/Source/Data/Base/PouringName.swift
+++ PouringFarm/Source/Data/Base/PouringName.swift
@@ -21,10 +21,16 @@ enum PouringName: String, CaseIterable {
         }
     }
     
-    static func pouringsList() -> [(type: PouringName, name: String, image: String)] {
+    static func pouringsList(total: Int) -> [PouringInfo] {
 
-        let pourings = PouringName.allCases + Array(repeating: PouringName.준비중, count: 26)
-        let list = pourings.map { ($0, $0.rawValue, $0.imageName(10)) }
+        let pourings = PouringName.allCases + Array(repeating: PouringName.준비중, count: (total - 4))
+        let list = pourings.map {
+            PouringInfo(
+                type: $0,
+                name: $0.rawValue,
+                image: $0.imageName(10)
+            )
+        }
         
         return list
     }
@@ -44,5 +50,5 @@ private let pouringIntro: [PouringName: String] = [
     PouringName.따끔푸링: "안녕하세요! 저는 따끔따끔 푸링이에요:>\n저는 뾰족뾰족 가시로 저를 괴롭히는 못된 슬라임들을 따끔하게 혼내줍니다!\n앗! 그래도 절 키워주신다면 지켜드릴게요S2",
     PouringName.헤실푸링: "헤헤헤헤헤%^^%\n이름에서부터 보이겠지만 저는 실없이 잘 웃는 헤실헤실 푸링이에요 헤헤..\n재미없는 농담을 하셔도 제가 항상 웃어드릴 수 있답니다 헤헤=_=",
     PouringName.반짝푸링: "나는야 꾸미기를 좋아하는 스타 슬라임~ 반짝반짝 푸링이야!\n주인공이 되고 싶다면 나와 함께해!\n내가 반짝반짝 후광을 비춰줄게!!",
-    PouringName.준비중: "새로운 푸링이가 준비중입니다!"
+    PouringName.준비중: "새로운 푸링이가 준비중입니다!\n빠른 시일내로 찾아올테니 다른 푸링이를 선택해주세요!"
 ]
