d4d3c76f88558330046a790bfce7cdf24572471e
onesunny2 <ippne996@gmail.com>
2025-02-23 01:32:20 +0900

    [design] 심볼이미지의 깔끔한 코드사용을 위한 열거형 생성

---
+++ PouringFarm/Source/Data/Base/SymbolImage.swift
@@ -0,0 +1,28 @@
+//
+//  SymbolImage.swift
+//  PouringFarm
+//
+//  Created by Lee Wonsun on 2/23/25.
+//
+
+import UIKit
+
+enum SymbolImage {
+    case 프로필
+    case 밥
+    case 물
+    case 이름설정
+    case 푸링이변경
+    case 초기화
+    
+    var imgName: UIImage {
+        switch self {
+        case .프로필: return UIImage(systemName: "person.crop.circle") ?? UIImage()
+        case .밥: return UIImage(systemName: "leaf.circle") ?? UIImage()
+        case .물: return UIImage(systemName: "drop.circle") ?? UIImage()
+        case .이름설정: return UIImage(systemName: "pencil") ?? UIImage()
+        case .푸링이변경: return UIImage(systemName: "moon.fill") ?? UIImage()
+        case .초기화: return UIImage(systemName: "arrow.clockwise") ?? UIImage()
+        }
+    }
+}
---
+++ PouringFarm/Source/Presentation/Home/HomeView/HomeView.swift
@@ -0,0 +1,8 @@
+//
+//  HomeView.swift
+//  PouringFarm
+//
+//  Created by Lee Wonsun on 2/23/25.
+//
+
+import Foundation
