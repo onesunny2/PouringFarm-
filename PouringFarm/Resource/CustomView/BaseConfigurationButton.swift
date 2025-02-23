deb5d1908f35bb9924e3b9c2d6b0056a36352607
onesunny2 <ippne996@gmail.com>
2025-02-23 19:27:30 +0900

    [chore] attributedTitle 적용

--- PouringFarm/Resource/CustomView/BaseConfigurationButton.swift
+++ PouringFarm/Resource/CustomView/BaseConfigurationButton.swift
@@ -12,15 +12,23 @@ final class BaseConfigurationButton: UIButton {
     init(buttonImage: UIImage, buttonTitle: String) {
         super.init(frame: .zero)
         
+        let inset: CGFloat = 6
+        
+        let container = AttributeContainer().font(.systemFont(ofSize: 15, weight: .semibold))
+        let attributedTitle = AttributedString(buttonTitle, attributes: container)
+        
         var config = UIButton.Configuration.filled()
         config.image = buttonImage
-        config.title = buttonTitle
+        config.attributedTitle = attributedTitle
         config.baseBackgroundColor = .prBackground
         config.baseForegroundColor = .prMain
         config.cornerStyle = .medium
         config.background.strokeColor = .prMain
         config.background.strokeWidth = 1
         config.imagePadding = 2
+        config.contentInsets = NSDirectionalEdgeInsets(top: inset, leading: inset, bottom: inset, trailing: inset)
+        
+        configuration = config
     }
     
     @available(*, unavailable)
