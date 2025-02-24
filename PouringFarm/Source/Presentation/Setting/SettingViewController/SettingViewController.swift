b1fb72c4f08001d9685a222ecd437cf83954a793
onesunny2 <ippne996@gmail.com>
2025-02-24 15:08:55 +0900

    [chore] 닉네임 변경 후 tableview의 detailText도 자동으로 변경되도록 수정

--- PouringFarm/Source/Presentation/Setting/SettingViewController/SettingViewController.swift
+++ PouringFarm/Source/Presentation/Setting/SettingViewController/SettingViewController.swift
@@ -59,12 +59,19 @@ final class SettingViewController: UIViewController {
             ) { (row, element, cell) in
                 cell.textLabel?.font = .systemFont(ofSize: 14, weight: .semibold)
                 cell.textLabel?.text = element.title
-                cell.detailTextLabel?.text = element.detail
                 cell.imageView?.image = element.image
                 cell.tintColor = .prNavi
                 cell.accessoryType = .disclosureIndicator
                 cell.backgroundColor = .clear
                 cell.separatorInset.left = .zero
+                
+                guard row == 0 else { return }
+                SavingInfo.$daejang
+                    .bind(with: self) { owner, name in
+                        guard let name else { return }
+                        cell.detailTextLabel?.text = name
+                    }
+                    .disposed(by: self.disposeBag)
             }
             .disposed(by: disposeBag)
     }
