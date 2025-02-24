82fb6542b04d5d58d577e15f2a1924ea8e90abbd
onesunny2 <ippne996@gmail.com>
2025-02-24 13:45:13 +0900

    [feat] 닉네임 변경하기 화면 UI 구현 및 로직 구현

--- PouringFarm/Source/Presentation/ChangeName/ChangeNameViewController/ChangeNameViewController.swift
+++ PouringFarm/Source/Presentation/ChangeName/ChangeNameViewController/ChangeNameViewController.swift
@@ -6,24 +6,56 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
-class ChangeNameViewController: UIViewController {
+final class ChangeNameViewController: UIViewController {
+    
+    private let mainView = ChangeNameView()
+    private let viewModel = ChangeViewModel()
+    private let disposeBag = DisposeBag()
+    
+    private let rightBarButton = UIBarButtonItem(title: "저장")
+    
+    override func loadView() {
+        view = mainView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        basicSetting()
+        bind()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        
+        view.endEditing(true)
     }
-    */
+    
+    private func bind() {
+        
+        let input = ChangeViewModel.Input(
+            tappedSaveButton: rightBarButton.rx.tap,
+            newNickname: mainView.textfield.rx.text.orEmpty
+        )
+        let output = viewModel.transform(input: input)
+        
+        output.dismissAction
+            .bind(with: self) { owner, _ in
+                owner.navigationController?.popViewController(animated: true)
+            }
+            .disposed(by: disposeBag)
+    }
+}
 
+extension ChangeNameViewController {
+    
+    private func basicSetting() {
+        navigationItem.title = NavigationTitle.대장이름설정.text
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.prNavi]
+        navigationController?.navigationBar.tintColor = .prNavi
+        navigationItem.rightBarButtonItem = rightBarButton
+    }
 }
--- PouringFarm/Source/Presentation/ChangeName/ChangeViewModel/ChangeViewModel.swift
+++ PouringFarm/Source/Presentation/ChangeName/ChangeViewModel/ChangeViewModel.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+import RxCocoa
+import RxSwift
+
+final class ChangeViewModel: BaseViewModel {
+    
+    var disposeBag: DisposeBag = DisposeBag()
+    
+    let textfieldText = BehaviorRelay(value: "")
+    
+    struct Input {
+        let tappedSaveButton: ControlEvent<()>
+        let newNickname: ControlProperty<String>
+    }
+    
+    struct Output {
+        let dismissAction: PublishRelay<Void>
+    }
+    
+    init() {
+        
+    }
+    
+    func transform(input: Input) -> Output {
+        
+        let dismissAction = PublishRelay<Void>()
+
+        input.tappedSaveButton
+            .withLatestFrom(input.newNickname)
+            .bind(with: self) { owner, text in
+                
+                guard text.count >= 2, text.count <= 6 else { return }
+                SavingInfo.daejang = text
+                dismissAction.accept(())
+            }
+            .disposed(by: disposeBag)
+        
+        return Output(dismissAction: dismissAction)
+    }
+}
